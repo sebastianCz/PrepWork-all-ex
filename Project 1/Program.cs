ba920c4b5faeda4e46036a117193f07ae235771f
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-18 11:38:22 +0200

    Learned data types

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-Console.WriteLine("This is the first line");
-Console.WriteLine("This is the second line");
+Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(12.30m);
+Console.WriteLine(true);
+Console.WriteLine(false);
