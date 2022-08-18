7767ca3b2348ee12cb364892acafa862239a470b
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-18 12:01:26 +0200

    Learned about Verbatim strings

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,6 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-string name = "BOB";
-int msgNr = 3;
-var temperature = 34.4;
-Console.WriteLine("Hello," + name + " You have" + msgNr + " messages in your inbox. The temperature is " + temperature + " celsius");
+﻿Console.WriteLine("Generating invoices for customer \"ABC Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+Console.WriteLine(@"c:\invoices\app.exe -j");
