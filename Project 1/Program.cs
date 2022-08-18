86e9396784c3d3feb16153201cbfd5baedd9cb11
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-18 11:47:58 +0200

    Declared a variable without defining type

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 string firstName;
+ 
     firstName = "BOB";
 Console.WriteLine(firstName);
 firstName = "Emily";
 Console.WriteLine(firstName);
+var message = "Hello world!";
+Console.WriteLine(message);
+
