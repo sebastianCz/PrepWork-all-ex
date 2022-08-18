7785e39f6b8cdffec387772b594da4b75e12de1f
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-18 11:42:52 +0200

    declared variables

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-Console.WriteLine('b');
-Console.WriteLine(123);
-Console.WriteLine(12.30m);
-Console.WriteLine(true);
-Console.WriteLine(false);
+String firstName;
+char userOption
+int gameScore
+decimal particlesPerMillion
+bool processedCustomer
