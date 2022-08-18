6fc340031bf53a46308dd6b4d810c6d9b22867b9
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-18 11:45:07 +0200

    Declared variable and changed values

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-String firstName;
-char userOption
-int gameScore
-decimal particlesPerMillion
-bool processedCustomer
+string firstName;
+    firstName = "BOB";
+Console.WriteLine(firstName);
+firstName = "Emily";
+Console.WriteLine(firstName);
