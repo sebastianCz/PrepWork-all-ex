91d0cea0b4b94d857bd77aa9203daa16627bbde2
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-18 12:26:21 +0200

    basic math

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,7 +1,13 @@
-﻿int firstNumber = 12;
-int secondNumber = 7;
-Console.WriteLine(firstNumber + secondNumber);
+﻿decimal decimalQuotient = 7.0m / 5;
 
-string firstName = "Bob";
-int widgetsSold = 7;
-Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+ 
+
+Console.WriteLine("Decimal quotient: " + decimalQuotient);
+
+Console.WriteLine("Modulus of 200 / 5 : " + (200 % 5));
+Console.WriteLine("Modulus of 7 / 5 : " + (7 % 5));
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
