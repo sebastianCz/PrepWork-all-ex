fdf7c2b333d2911cd5516b324746acbec0ab9956
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-18 12:04:00 +0200

    concating works like in JS

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,10 +1,3 @@
-﻿Console.WriteLine("Generating invoices for customer \"ABC Corp\" ...\n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
-Console.Write(@"c:\invoices");
-
-// To generate Japanese invoices:
-// Nihon no seikyū-sho o seisei suru ni wa:
-Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
-Console.WriteLine(@"c:\invoices\app.exe -j");
+﻿string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
