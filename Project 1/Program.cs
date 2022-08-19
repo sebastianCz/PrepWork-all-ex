8fd802787f791399be23b5448e98093d9939b541
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-19 16:51:34 +0200

    conversions b

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,10 +1,10 @@
-﻿//int first = 2;
-//string second = "4";
-//int result = first + second;
-//Console.WriteLine(result);
+﻿string first = "5";
+string second = "7";
+int sum = int.Parse(first) + int.Parse(second);
+Console.WriteLine(sum);
 
-int myInt = 3;
-Console.WriteLine($"int:{myInt}");
 
-decimal myDecimal = myInt;
-Console.WriteLine($"decimal:{myDecimal}");
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1)*Convert.ToInt32(value2);
+Console.WriteLine(result);
