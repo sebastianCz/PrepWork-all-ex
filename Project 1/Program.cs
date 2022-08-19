cdf0617b348ace943529bff649bc6c0b3506993f
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-19 13:09:31 +0200

    learned comparisons

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*string value1 = "a";
+string value2 = "A";
+
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+*/
+
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+
+Console.WriteLine(myValue != "a");
+
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
