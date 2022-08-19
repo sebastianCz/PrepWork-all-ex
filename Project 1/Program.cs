4026938856a248a371a8a47203edbb10e8dbe0bb
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-19 15:05:15 +0200

    iterrations

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,54 +1,14 @@
-﻿// SKU = Stock Keeping Unit
-string sku = "01-MN-L";
+﻿string[] names = { "Alex", "Eddie", "David", "Michael" };
 
-string[] product = sku.Split('-');
 
-string type = "";
-string color = "";
-string size = "";
-
-switch (product[0]  )
-{
-
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type ="T-shirt";
-        break;
-    case "03":
-        type = "sweatpants";
-        break;
-    
-
-}
-
-switch (product[1]  )
-{
-
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
+for (int i = 0; i < names.Length; i++)
  
-}
+    if (names[i] == "David") names[i] = "Sammy";
 
-switch (product[2])
-{
+foreach (var name in names) { Console.WriteLine(name); }
 
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
 
-}
 
-  
-Console.WriteLine($"Product: {size} {color} {type}");
+
+
+ 
