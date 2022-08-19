0f7c705b71adbf1d04ee4b10656387857d08fde6
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-19 16:55:22 +0200

    try parse vs parse

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,10 +1,9 @@
-﻿string first = "5";
-string second = "7";
-int sum = int.Parse(first) + int.Parse(second);
-Console.WriteLine(sum);
-
-
-string value1 = "5";
-string value2 = "7";
-int result = Convert.ToInt32(value1)*Convert.ToInt32(value2);
-Console.WriteLine(result);
+﻿string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
