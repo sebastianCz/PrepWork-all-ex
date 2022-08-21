3c0934af805d9e858635217fceee754d63d86942
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-22 01:01:00 +0200

    array manipulation

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,38 +1,128 @@
-﻿int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+﻿
 
-int result1 = 0;
-float result3 = 0;
+//Console.WriteLine("Sorted...");
+/*Array.Sort(pallets);
 
-// Your code here to set result
+foreach(var pallet in pallets)
+{
 
-value1 = 12;
-value2 = 6.2m;
-value3 = 4.3f;
+    Console.WriteLine($"--{pallet}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("Reversed..");
+Array.Reverse(pallets);
+
+foreach(var pallet in pallets)
+{
+
+    Console.WriteLine($"--{pallet}");
+}
+*/
+/*string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}*/
+/*
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("");
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2...count:{pallets.Length}");
+
+ foreach(var pallet in pallets)
+{
+
+    Console.WriteLine($"---{pallet}");
+}
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6, count:{pallets.Length}");
+pallets[4] = "CO1";
+pallets[5] = "CO2";
+
+foreach(var pallet in pallets)
+{
+
+    Console.WriteLine($"--{pallet}");
+
+}
+
+Console.Write("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"REsizing3...count :{pallets.Length}");
+
+foreach ( var pallet in pallets){
+
+    Console.WriteLine($"--{pallet}");
+
+}
+*/
+/*
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+//string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach(string item in items)
+{
+
+    Console.WriteLine(item); 
+
+}
+*/
+/*
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] worlds = pangram.Split(null);
+string[] result = {};
+Array.Resize(ref result, worlds.Length);
 
 
-result1 = value1 / (int)value2;
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+for( int x = 0; worlds.Length > x;x++)
+{
+    char[] charArray = worlds[x].ToCharArray();
+    
+    Array.Reverse(charArray);
+    result[x] = String.Join("", charArray);
+    // var reverseResult = Array.Reverse(charArray);
+    Console.Write(" "+result[x].ToString());
 
-// Your code here to set result
-value1 = 12;
-value2 = 6.2m;
-value3 = 4.3f;
+}*/
 
-decimal result2 = value2 / (decimal)value3;
+string pangram = "The quick brown fox jumps over the lazy dog";
 
- 
+string[] message = pangram.Split(' ');
+string[] newMessage = new string[message.Length];
 
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
 
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-// Your code here to set result3
-
-value1 = 12;
-value2 = 6.2m;
-value3 = 4.3f;
-
-result3 = value3 / value1;
-result3 = (float)result3;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
