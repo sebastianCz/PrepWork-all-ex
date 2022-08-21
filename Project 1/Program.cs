177af5b181061e51ef5f8c20d36acdbf27c73ad6
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-22 01:44:28 +0200

    Formating arrays part 2

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -112,7 +112,7 @@ for( int x = 0; worlds.Length > x;x++)
 
 }*/
 
-string pangram = "The quick brown fox jumps over the lazy dog";
+/*string pangram = "The quick brown fox jumps over the lazy dog";
 
 string[] message = pangram.Split(' ');
 string[] newMessage = new string[message.Length];
@@ -125,4 +125,32 @@ for (int i = 0; i < message.Length; i++)
 }
 
 string result = String.Join(" ", newMessage);
-Console.WriteLine(result);
+Console.WriteLine(result);*/
+
+using System;
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderArray = orderStream.Split(',');
+string[] result = { " " };
+int count = 0;
+
+for (int x = 0; x < orderArray.Length; x++) 
+{
+    if (orderArray[x].Contains("B")){
+
+        count++;
+       Array.Resize(ref result, count);
+        result[count - 1] = orderArray[x];
+
+
+        Console.WriteLine(count +" result length"+result.Length+ result[count - 1]);
+    }
+    
+
+}
+
+Array.Sort(result);
+List<string> text = result.ToList();
+text.ForEach(Console.WriteLine);
+
