449811a0ebf321ec2b7026dc07f4191a9bf6e04a
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-22 03:44:19 +0200

    substring

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,50 +1,15 @@
-﻿/*using Microsoft.VisualBasic;
+﻿string message = "(What if) there are (more than) one (set of parentheses)?";
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
 
-Dear Mr.Jones,
-As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
 
-Currently, you own 2,975,000.00 shares at a return of 12.75 %.
-
-Our new product, Glorious Future offers a return of 13.13 %.  Given your current volume, your potential profit would be ¤63,000,000.00.
-
-Here's a quick comparison:
-
-Magic Yield         12.75 %   ¤55,000,000.00      
-Glorious Future     13.13 %   ¤63,000,000.00
-
-*/
-
-
-string customerName = "Mr. Jones";
-
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
-
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
-
-string message =  $"Dear {customerName}, " +
-    $"\n As a customer of our {currentProduct} offering we are  excited to tell you about a new financial product that would dramatically increase your return." +
-    $"\n Currently, you own {currentShares:N} shares at a return of {currentReturn:P2}." +
-    $"\n Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}\r\n";
-Console.Write(message);
-
-
-Console.WriteLine("Here's a quick comparison:\n");
-
-string comparisonMessage = "";
-
-comparisonMessage = currentProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
-
-comparisonMessage += "\n";
-comparisonMessage += newProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
- 
- 
-Console.WriteLine(comparisonMessage);
+    // Note how we use the overload of Substring to return only the remaining 
+    // unprocessed message:
+    message = message.Substring(closingPosition + 1);
+}
