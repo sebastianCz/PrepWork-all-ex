46b22d9a9393a1209646c484ff609f516dd6ebfd
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-22 02:54:21 +0200

    text formatting

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,10 +1,50 @@
-﻿ 
-int invoiceNumber = 1201;
-decimal productMeasurement = 25.4568m;
-decimal subtotal = 2750.00m;
-decimal taxPercentage = .15825m;
-decimal total = 3185.19m;
-Console.WriteLine($"Invoice Number: {invoiceNumber}");
-Console.WriteLine($"   Measurement: {productMeasurement:N3} mg");
-Console.WriteLine($"     Sub Total: {subtotal:C}");
-Console.WriteLine($"     Total Due: {total:C}");
+﻿/*using Microsoft.VisualBasic;
+
+Dear Mr.Jones,
+As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.
+
+Currently, you own 2,975,000.00 shares at a return of 12.75 %.
+
+Our new product, Glorious Future offers a return of 13.13 %.  Given your current volume, your potential profit would be ¤63,000,000.00.
+
+Here's a quick comparison:
+
+Magic Yield         12.75 %   ¤55,000,000.00      
+Glorious Future     13.13 %   ¤63,000,000.00
+
+*/
+
+
+string customerName = "Mr. Jones";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+string message =  $"Dear {customerName}, " +
+    $"\n As a customer of our {currentProduct} offering we are  excited to tell you about a new financial product that would dramatically increase your return." +
+    $"\n Currently, you own {currentShares:N} shares at a return of {currentReturn:P2}." +
+    $"\n Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}\r\n";
+Console.Write(message);
+
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+ 
+ 
+Console.WriteLine(comparisonMessage);
