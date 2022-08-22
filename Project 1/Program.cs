1d9b89b644bbc2f08490416ddd900cb689143f73
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2022-08-22 04:48:46 +0200

    working with numbers

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -1,2 +1,94 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//WorkWithIntegers();
+
+OrderPrecedence();
+
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+    //substraction
+
+    c = a - b;
+    Console.WriteLine(c);
+
+    //multiplaction
+    c = a * b;
+    Console.WriteLine(c);
+    //Division 
+
+    c = a / b;
+    Console.WriteLine(c);
+
+}
+
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
+
+
+}
+
+void testLimits()
+{
+    int a = 7;
+    int b = 4;
+    int c = 3;
+    int d = (a + b) / c;
+    int e = (a + b) % c;
+    Console.WriteLine($"quotient: {d}");
+    Console.WriteLine($"remainder: {e}");
+
+    int max = int.MaxValue;
+    int min = int.MinValue;
+    Console.WriteLine($"The range of integers is {min} to {max}");
+
+    int what = max + 3;
+    Console.WriteLine($"An example of overflow:{what}");
+}
+void workWithDoubles()
+{
+    double a = 5;
+    double b = 4;
+    double c = 2;
+    double d = (a + b) / c;
+    Console.WriteLine(d);
+
+    double e = 19;
+    double f = 23;
+    double g = 8;
+    double h = (e + f) / g;
+    Console.WriteLine(h);
+
+    double max = double.MaxValue;
+    double min = double.MinValue;
+    Console.WriteLine($"The range of double is {min} to {max}");
+}
+
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min}to {max}");
+
+//radius of a circle of 2.5 cm in diameter
+
+double radius = 2.5;
+double area = (radius * radius) * Math.PI;
+Console.WriteLine(area);
